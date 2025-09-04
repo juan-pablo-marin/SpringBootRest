44ca6ae12ad62855613051850fbba65097a45ce1
juan-pablo-marin <jupamarin@gmail.com>
2025-09-04 10:06:32 -0500

    se dockeriza proyecto

--- src/main/java/com/aplication/rest/SpringBootRest/entities/makerEntitie/application/ports/in/GetByIdUseCase.java
+++ src/main/java/com/aplication/rest/SpringBootRest/entities/makerEntitie/application/ports/in/GetByIdUseCase.java
@@ -1,6 +1,5 @@
 package com.aplication.rest.SpringBootRest.entities.makerEntitie.application.ports.in;
 
-import com.aplication.rest.SpringBootRest.entities.makerEntitie.domain.model.Maker;
 import com.aplication.rest.SpringBootRest.entities.makerEntitie.dto.MakerDTO;
 
 import java.util.Optional;
